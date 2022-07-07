ff9779ad798201351be001a255473d5b33f1cd2e
rpKJ <20037535@myrp.edu.sg>
2022-07-07 11:32:16 +0800

    commit message goes here

--- web/index.php
+++ web/index.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Testing lighthouse</title>
 <html lang="en">
+    <style>
+        i {
+            font-size: 40px;
+        }
+    </style>
 <head>
 </head>
 <body>
