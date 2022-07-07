ddff0d5dbe122c0a436bca7927afb61b999f2a47
rpKJ <20037535@myrp.edu.sg>
2022-07-07 12:17:04 +0800

    commit message goes here

--- web/index.php
+++ web/index.php
@@ -1,13 +1,19 @@
+<!-- fix :Document requires doctype -->
 <!DOCTYPE html>
+<!-- fix :SEO Requires -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- fix : Document doesn't have title element -->
 <title>Testing lighthouse</title>
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
+    <!--Document requires legible font sizes-->
     <style>
         i {
             font-size: 40px;
         }
     </style>
 <head>
+    <!-- fix :Document Requires meta name and description-->
 <meta
   name="description"
   content="Mary's simple recipe for maple bacon donuts
--- web/test.php
+++ web/test.php
@@ -1,5 +1,24 @@
+<!-- fix :Document requires doctype -->
+<!DOCTYPE html>
+<!-- fix :SEO Requires -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- fix : Document doesn't have title element -->
+<title>Testing lighthouse</title>
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
+    <!--Document requires legible font sizes-->
+    <style>
+        i {
+            font-size: 40px;
+        }
+    </style>
 <head>
+    <!-- fix :Document Requires meta name and description-->
+<meta
+  name="description"
+  content="Mary's simple recipe for maple bacon donuts
+           makes a sticky, sweet treat with just a hint
+           of salt that you'll keep coming back for.">
 </head>
 <body>
 <i>Hello World!</i>
